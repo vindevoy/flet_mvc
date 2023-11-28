6784bf5148587c139f351260af4dbee28b441ada
Yves Vindevogel <vindevoy@users.noreply.github.com>
2023-11-28 01:37:11 +0100

    Added basic datatable

--- tests/contact_app/main.py
+++ tests/contact_app/main.py
@@ -3,7 +3,7 @@ from pathlib import Path
 from app import FletMVCApplication
 from contact_app.controller import ContactsController
 from contact_app.model import ContactsModel
-from contact_app.view import ContactsView
+from contact_app.view import ContactsListView
 from module import FletMVCModule
 
 current_path = Path(__file__).parent
@@ -12,10 +12,10 @@ db_path = current_path.joinpath("contacts.db").absolute()
 app = FletMVCApplication("Contact MVC")
 app.bind_database(f"sqlite:///{db_path}")
 
-contacts = FletMVCModule(model_class=ContactsModel,
-                         view_class=ContactsView,
-                         controller_class=ContactsController)
+contact_list = FletMVCModule(model_class=ContactsModel,
+                             view_class=ContactsListView,
+                             controller_class=ContactsController)
 
-app.add_route("/", contacts)
+app.add_route("/", contact_list)
 
 app.run()
--- tests/contact_app/view.py
+++ tests/contact_app/view.py
@@ -3,7 +3,7 @@ import flet as ft
 from flet_mvc.view import FletMVCView
 
 
-class ContactsView(FletMVCView):
+class ContactsListView(FletMVCView):
     def __init__(self):
         super().__init__()
 
@@ -12,8 +12,26 @@ class ContactsView(FletMVCView):
         self.view = ft.Ref[ft.View]()
 
     def build(self) -> ft.View:
+        data = self.model.get_all()
+
+        cols = [ft.DataColumn(ft.Text(c)) for c in ["ID", "Firstname", "Lastname", "Login"]]
+        rows = []
+
+        for rec in data:
+            row = ft.DataRow(
+                cells=[
+                    ft.DataCell(ft.Text(rec.id)),
+                    ft.DataCell(ft.Text(rec.firstname)),
+                    ft.DataCell(ft.Text(rec.lastname)),
+                    ft.DataCell(ft.Text(rec.login))
+                ],
+            )
+
+            rows.append(row)
+
+        self.datatable = ft.DataTable(columns=cols, rows=rows, expand=True)  # TODO: full width
         self.button = ft.ElevatedButton(ref=self.button, col=2, text="Close",
                                         on_click=self.controller.close_application)
-        self.view = ft.View(controls=[self.button])
+        self.view = ft.View(controls=[self.datatable, self.button])
 
         return self.view
